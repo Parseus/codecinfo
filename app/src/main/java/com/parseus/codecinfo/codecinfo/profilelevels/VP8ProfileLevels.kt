94ca0b0af5e7d0fd828eb8ad1b07a98d1c989b6b
Parseus <nawrot.krzysztof@gmail.com>
2018-07-06 20:38:45 +0200

    Added more VP8 profiles.

--- app/src/main/java/com/parseus/codecinfo/codecinfo/profilelevels/VP8ProfileLevels.kt
+++ app/src/main/java/com/parseus/codecinfo/codecinfo/profilelevels/VP8ProfileLevels.kt
@@ -2,7 +2,12 @@ package com.parseus.codecinfo.codecinfo.profilelevels
 
 enum class VP8Profiles(val value: Int) {
 
-    VP8ProfileMain(0x01);
+    VP8ProfileMain(0x01),
+    VP8ProfileAdvanced(0x02),
+    VP8ProfileVersion0(0x04),
+    VP8ProfileVersion1(0x08),
+    VP8ProfileVersion2(0x10),
+    VP8ProfileVersion3(0x20);
 
     companion object {
         fun from(findValue: Int): String? = try {

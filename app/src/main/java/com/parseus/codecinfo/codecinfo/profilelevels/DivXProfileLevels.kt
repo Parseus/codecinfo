1b0533b90ecf05f5133c28401fe1b7ad57931b90
Parseus <nawrot.krzysztof@gmail.com>
2018-07-20 22:55:29 +0200

    Added DivX profiles for 1080p and Plus HD.

--- app/src/main/java/com/parseus/codecinfo/codecinfo/profilelevels/DivXProfileLevels.kt
+++ app/src/main/java/com/parseus/codecinfo/codecinfo/profilelevels/DivXProfileLevels.kt
@@ -8,6 +8,8 @@ enum class DivXProfiles(val value: Int) {
     QOMX_VIDEO_DivXProfileMT(0x04),
     QOMX_VIDEO_DivXProfileHT(0x08),
     QOMX_VIDEO_DivXProfileHD(0x10),
+    QOMX_VIDEO_DivXProfileFullHD(0x20),
+    QOMX_VIDEO_DivXProfilePlusHD(0x40),
     QOMX_VIDEO_DivXProfileMax(0x7FFFFFFF);
 
     companion object {

c169528d0d03bfaeb750b93f51d04d3508c256bb
Parseus <nawrot.krzysztof@gmail.com>
2018-07-20 19:33:43 +0200

    Added 4 new color formats from Qualcomm.

--- app/src/main/java/com/parseus/codecinfo/codecinfo/colorformats/QualcommColorFormat.kt
+++ app/src/main/java/com/parseus/codecinfo/codecinfo/colorformats/QualcommColorFormat.kt
@@ -7,8 +7,11 @@ enum class QualcommColorFormat (val value: Int) {
     QOMX_COLOR_FormatYUV420PackedSemiPlanar16m2ka(0x7FA30C02),
     QOMX_COLOR_FormatYUV420PackedSemiPlanar64x32Tile2m8ka(0x7FA30C03),
     QOMX_COLOR_FORMATYUV420PackedSemiPlanar32m(0x7FA30C04),
-    QOMX_COLOR_FORMATYUV420PackedSemiPlanar32mMultiView(0x7FA30C05);
-    //TODO: Find info about 0x7FA30C06
+    QOMX_COLOR_FORMATYUV420PackedSemiPlanar32mMultiView(0x7FA30C05),
+    QOMX_COLOR_FORMATYUV420PackedSemiPlanar32mCompressed(0x7FA30C06),
+    QOMX_COLOR_Format32bitRGBA8888(0x7FA30C07),
+    QOMX_COLOR_Format32bitRGBA8888Compressed(0x7FA30C08),
+    QOMX_COLOR_FormatAndroidOpaque(0x7F000789);
 
     companion object {
         fun from(findValue: Int): String? = QualcommColorFormat.values().find { it.value == findValue }?.name

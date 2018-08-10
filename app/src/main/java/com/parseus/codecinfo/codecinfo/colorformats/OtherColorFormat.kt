0a82bddbc1d1b0b2ab54645b30d7be4c35294c85
Parseus <nawrot.krzysztof@gmail.com>
2018-08-10 21:10:56 +0200

    Added OMX_COLOR_FormatYUV420MBPackedSemiPlanar.

--- app/src/main/java/com/parseus/codecinfo/codecinfo/colorformats/OtherColorFormat.kt
+++ app/src/main/java/com/parseus/codecinfo/codecinfo/colorformats/OtherColorFormat.kt
@@ -9,6 +9,7 @@ enum class OtherColorFormat(val value: Int) {
 
     // Sony
     OMX_STE_COLOR_FormatYUV420PackedSemiPlanarMB(0x7FA00000),
+    OMX_COLOR_FormatYUV420MBPackedSemiPlanar(0x7FFFFFFE),
 
     // Texas Instruments
     OMX_TI_COLOR_FormatYUV420PackedSemiPlanarInterlaced(0x7F000001),

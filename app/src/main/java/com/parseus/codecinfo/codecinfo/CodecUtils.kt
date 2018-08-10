7e18ddf0182f310eae001656ba352a35b0a920c5
Parseus <nawrot.krzysztof@gmail.com>
2018-08-10 20:51:24 +0200

    Fixed H.263 not being recognized by some FLV codecs.

--- app/src/main/java/com/parseus/codecinfo/codecinfo/CodecUtils.kt
+++ app/src/main/java/com/parseus/codecinfo/codecinfo/CodecUtils.kt
@@ -437,7 +437,7 @@ object CodecUtils {
                     level = DolbyVisionLevels.from(it.level) ?: "$unknownString (${it.level.toHexHstring()})"
                 }
                 codecId.contains("3gpp") || codecId.contains("sorenson")
-                        || codecName.contains("sorenson", true) -> {
+                        || codecName.contains("flv") -> {
                     profile = H263Profiles.from(it.profile) ?: "$unknownString (${it.profile.toHexHstring()})"
                     level = H263Levels.from(it.level) ?: "$unknownString (${it.level.toHexHstring()})"
                 }

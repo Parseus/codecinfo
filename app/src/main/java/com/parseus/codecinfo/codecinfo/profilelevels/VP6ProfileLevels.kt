28df2bab0faa95171ff5b89feeec8136b91dffee
Parseus <nawrot.krzysztof@gmail.com>
2018-08-10 20:38:10 +0200

    Added OMX_VIDEO_VP6ProfileHeightenedSharpness.

--- app/src/main/java/com/parseus/codecinfo/codecinfo/profilelevels/VP6ProfileLevels.kt
+++ app/src/main/java/com/parseus/codecinfo/codecinfo/profilelevels/VP6ProfileLevels.kt
@@ -5,6 +5,7 @@ enum class VP6Profiles(val value: Int) {
 
     OMX_VIDEO_VP6ProfileSimple(0x01),
     OMX_VIDEO_VP6ProfileAdvanced(0x02),
+    OMX_VIDEO_VP6ProfileHeightenedSharpness(0x03),
     OMX_VIDEO_VP6ProfileMax(0x7FFFFFFF);
 
     companion object {

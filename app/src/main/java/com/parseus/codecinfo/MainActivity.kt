a6b4704a304f5af9b21b2f3db7ca2e901df4c566
Parseus <nawrot.krzysztof@gmail.com>
2019-02-25 21:53:45 +0100

    Enabled immersive mode.

--- app/src/main/java/com/parseus/codecinfo/MainActivity.kt
+++ app/src/main/java/com/parseus/codecinfo/MainActivity.kt
@@ -2,10 +2,12 @@ package com.parseus.codecinfo
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Looper
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
@@ -130,6 +132,22 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentByTag("SINGLE_PANE_DETAILS")?.let {
                 (it as DialogFragment).dialog?.takeIf { dialog -> dialog.isShowing }?.dismiss()
             }
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) setImmersiveMode()
+    }
+
+    private fun setImmersiveMode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
         }
     }
 

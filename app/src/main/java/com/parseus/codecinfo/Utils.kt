f300efc77db3d976f4cc6888432a3ceeec7a5fd2
Krzysztof Nawrot <nawrot.krzysztof@gmail.com>
2020-08-11 21:43:29 +0200

    Safeguarded a MIUI check
    Better safe than sorry.

--- app/src/main/java/com/parseus/codecinfo/Utils.kt
+++ app/src/main/java/com/parseus/codecinfo/Utils.kt
@@ -5,14 +5,18 @@ import android.os.Build
 import com.parseus.codecinfo.settings.DarkTheme
 
 @SuppressLint("PrivateApi")
-fun getSystemProperty(property: String): String {
-    val c = Class.forName("android.os.SystemProperties")
-    val get = c.getMethod("get", String::class.java)
-    return get.invoke(c, property) as String
+fun getSystemProperty(property: String): String? {
+    return try {
+        val c = Class.forName("android.os.SystemProperties")
+        val get = c.getMethod("get", String::class.java)
+        get.invoke(c, property) as? String
+    } catch (e: Exception) {
+        null
+    }
 }
 
 val isMiUi: Boolean
-    get() = getSystemProperty("ro.miui.ui.version.name").isNotEmpty()
+    get() = !getSystemProperty("ro.miui.ui.version.name").isNullOrEmpty()
 
 fun isBatterySaverDisallowed(): Boolean {
     return Build.VERSION.SDK_INT !in 21..28

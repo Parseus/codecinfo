f8c2537152fdef1f1fa7edd7499cf3223311297d
Krzysztof Nawrot <nawrot.krzysztof@gmail.com>
2020-12-10 00:24:46 +0100

    Fixed building the openSource variant.

--- app/src/openSource/java/com/parseus/codecinfo/ThirdPartyUtils.kt
+++ app/src/openSource/java/com/parseus/codecinfo/ThirdPartyUtils.kt
@@ -7,5 +7,5 @@ import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 
 fun initializeAppRating(activity: androidx.appcompat.app.AppCompatActivity) {}
-fun initializeSamsungGesture(context: Context, pager: ViewPager, tabLayout: TabLayout) {}
+fun initializeSamsungGesture(context: Context, pager: ViewPager2, tabLayout: TabLayout) {}
 fun destroySamsungGestures() {}

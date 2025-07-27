821b6ad02061ef29e08a563a405221b62c9157fd
Parseus <nawrot.krzysztof@gmail.com>
2025-07-27 11:28:08 +0200

    List the most common DRM vendors first so they can be found faster

--- app/src/main/java/com/parseus/codecinfo/data/drm/DrmVendor.kt
+++ app/src/main/java/com/parseus/codecinfo/data/drm/DrmVendor.kt
@@ -7,13 +7,11 @@ import java.util.UUID
 enum class DrmVendor(val uuid: UUID,
                      val properNameResId: Int = -1) {
 
-    AdobePrimeTime(UUID(-992507800680445872L, -7199381120623628549L)),
-    CiscoNDSVideoGuard(UUID(6801168765294362723L, -8541191043631480108L)),
+    // List the most common vendors first so they can be found faster, especially on Android 11+.
     ClearKey(UUID(0x1077EFECC0B24D02L, -0x531cc3e1ad1d04b5L),
-            R.string.drm_clearkey_pssh_name),
-    CoreCrypt(UUID(-5031789394971871680L, -5933022823712443662L)),
+        R.string.drm_clearkey_pssh_name),
     DashIfClearKey(UUID(-2129748144642739255L, 8654423357094679310L),
-            R.string.drm_clearkey_dash_if),
+        R.string.drm_clearkey_dash_if),
     /**
      * Not listed on DASH IF as a known DRM system; used by Netflix.
      * Apparently a CENC version of Apple's FairPlay,
@@ -21,18 +19,24 @@ enum class DrmVendor(val uuid: UUID,
      * See: https://developer.apple.com/forums/thread/6185
      */
     FairPlayNetflix(UUID(2985921618079337012L, -8332874748677350841L),
-            R.string.drm_fairplay_netflix_name),
-    Irdeto(UUID(-7029884071836333870L, -9176437742005367753L)),
-    Marlin(UUID(6801168765294362723L, -8541191043631480108L)),
-    MobiTv(UUID(7681262094454315298L, -7309041776357940493L)),
-    Nagra(UUID(-5930083867628189075L, -7670962396607644779L)),
-    PlayReady(UUID(-0x65fb0f8667bfbd7aL, -0x546d19a41f77a06bL)),
-    Verimatrix(UUID(-7338653513101981915L, -8305690818819724279L)),
+        R.string.drm_fairplay_netflix_name),
     Widevine(UUID(-0x121074568629b532L, -0x5c37d8232ae2de13L)) {
         override fun getVendorStringProperties(): Map<Int, String> = WIDEVINE_STRING_PROPERTIES
         override fun getVendorByteArrayProperties(): Map<Int, String> = WIDEVINE_BYTE_ARRAY_PROPERTIES
     },
-    WisePlay(UUID(0x3d5e6d359b9a41e8L, -0x47bc22c3918d3bd4L));
+    PlayReady(UUID(-0x65fb0f8667bfbd7aL, -0x546d19a41f77a06bL)),
+    WisePlay(UUID(0x3d5e6d359b9a41e8L, -0x47bc22c3918d3bd4L)),
+
+    // Those vendors are very uncommon on Android devices,
+    // but they're still here for the sake of completeness.
+    AdobePrimeTime(UUID(-992507800680445872L, -7199381120623628549L)),
+    CiscoNDSVideoGuard(UUID(6801168765294362723L, -8541191043631480108L)),
+    CoreCrypt(UUID(-5031789394971871680L, -5933022823712443662L)),
+    Irdeto(UUID(-7029884071836333870L, -9176437742005367753L)),
+    Marlin(UUID(6801168765294362723L, -8541191043631480108L)),
+    MobiTv(UUID(7681262094454315298L, -7309041776357940493L)),
+    Nagra(UUID(-5930083867628189075L, -7670962396607644779L)),
+    Verimatrix(UUID(-7338653513101981915L, -8305690818819724279L));
 
     open fun getVendorStringProperties(): Map<Int, String>? = null
     open fun getVendorByteArrayProperties(): Map<Int, String>? = null

34e7e0ba235fa6825fe720f6dbef58dab818bc3f
Parseus <nawrot.krzysztof@gmail.com>
2026-03-04 22:51:53 +0100

    HardenedWebView: more hardening, tweaks for bigger screens for

--- app/src/mobile/java/com/parseus/codecinfo/ui/HardenedWebView.kt
+++ app/src/mobile/java/com/parseus/codecinfo/ui/HardenedWebView.kt
@@ -27,6 +27,9 @@ class HardenedWebView : WebView {
             blockNetworkLoads = true
             cacheMode = WebSettings.LOAD_NO_CACHE
             saveFormData = false
+
+            loadWithOverviewMode = true
+            useWideViewPort = true
         }
         if (Build.VERSION.SDK_INT >= 26) {
             importantForAutofill = View.IMPORTANT_FOR_AUTOFILL_NO
@@ -82,42 +85,43 @@ class HardenedWebView : WebView {
     companion object {
         private const val CONTENT_SECURITY_POLICY =
             "default-src 'none'; " +
-                    "form-action 'none'; " +
-                    "img-src blob: 'self'; " +
-                    "script-src 'self'; " +
-                    "style-src 'self'; " +
-                    "frame-ancestors 'none'; " +
-                    "base-uri 'none'"
+            "form-action 'none'; " +
+            "connect-src 'none'; " +
+            "img-src blob: 'none'; " +
+            "script-src 'none'; " +
+            "style-src 'none'; " +
+            "frame-ancestors 'none'; " +
+            "base-uri 'none'"
         private const val PERMISSIONS_POLICY =
             "accelerometer=(), " +
-                    "ambient-light-sensor=(), " +
-                    "autoplay=(), " +
-                    "battery=(), " +
-                    "camera=(), " +
-                    "clipboard-read=(), " +
-                    "clipboard-write=(), " +
-                    "display-capture=(), " +
-                    "document-domain=(), " +
-                    "encrypted-media=(), " +
-                    "fullscreen=(), " +
-                    "gamepad=(), " +
-                    "geolocation=(), " +
-                    "gyroscope=(), " +
-                    "hid=(), " +
-                    "idle-detection=(), " +
-                    "interest-cohort=(), " +
-                    "magnetometer=(), " +
-                    "microphone=(), " +
-                    "midi=(), " +
-                    "payment=(), " +
-                    "picture-in-picture=(), " +
-                    "publickey-credentials-get=(), " +
-                    "screen-wake-lock=(), " +
-                    "serial=(), " +
-                    "speaker-selection=(), " +
-                    "sync-xhr=(), " +
-                    "usb=(), " +
-                    "xr-spatial-tracking=()"
+            "ambient-light-sensor=(), " +
+            "autoplay=(), " +
+            "battery=(), " +
+            "camera=(), " +
+            "clipboard-read=(), " +
+            "clipboard-write=(), " +
+            "display-capture=(), " +
+            "document-domain=(), " +
+            "encrypted-media=(), " +
+            "fullscreen=(), " +
+            "gamepad=(), " +
+            "geolocation=(), " +
+            "gyroscope=(), " +
+            "hid=(), " +
+            "idle-detection=(), " +
+            "interest-cohort=(), " +
+            "magnetometer=(), " +
+            "microphone=(), " +
+            "midi=(), " +
+            "payment=(), " +
+            "picture-in-picture=(), " +
+            "publickey-credentials-get=(), " +
+            "screen-wake-lock=(), " +
+            "serial=(), " +
+            "speaker-selection=(), " +
+            "sync-xhr=(), " +
+            "usb=(), " +
+            "xr-spatial-tracking=()"
     }
 
 }

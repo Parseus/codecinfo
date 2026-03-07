c0b7cb1e2d6fe980203e98b84d5a1e87c0c7ba6b
Parseus <nawrot.krzysztof@gmail.com>
2026-03-07 01:59:31 +0100

    Changed a doc link for MPEG-1 to the archived version

--- app/src/main/java/com/parseus/codecinfo/data/codecinfo/CodecUtils.kt
+++ app/src/main/java/com/parseus/codecinfo/data/codecinfo/CodecUtils.kt
@@ -691,7 +691,7 @@ private fun adjustMaxInputChannelCount(codecId: String, codecName: String, maxCh
         "audio/amr-wb-plus" -> 2
         "audio/dts" -> 8
         "audio/eac3" -> 16
-        // source: https://mpeg.chiariglione.org/standards/mpeg-1/audio
+        // source: https://web.archive.org/web/20240616183622/https://mpeg.chiariglione.org/standards/mpeg-1/audio
         "audio/mpeg", "audio/mpeg-l1", "audio/mpeg-l2" -> 2
         // Default to the platform limit, which is 30.
         else -> DEFAULT_MAX_INPUT_CHANNEL_LIMIT

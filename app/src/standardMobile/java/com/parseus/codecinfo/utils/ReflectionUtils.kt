7b1e24baa62e1010ff7f3d16bb4ce94b678379ea
Parseus <nawrot.krzysztof@gmail.com>
2026-03-18 23:43:02 +0100

    Use LSPass on older Android versions if possible

--- app/src/standardMobile/java/com/parseus/codecinfo/utils/ReflectionUtils.kt
+++ app/src/standardMobile/java/com/parseus/codecinfo/utils/ReflectionUtils.kt
@@ -2,11 +2,23 @@ package com.parseus.codecinfo.utils
 
 import android.os.Build
 import org.lsposed.hiddenapibypass.HiddenApiBypass
+import org.lsposed.hiddenapibypass.LSPass
 
 const val CAN_USE_REFLECTION_FOR_MCAPABILITIESINFO = true
 
 fun disableApiBlacklistOnPie() {
-    if (Build.VERSION.SDK_INT >= 28) {
-        HiddenApiBypass.addHiddenApiExemptions("Landroid/media/MediaCodecInfo\$CodecCapabilities")
-    }
+    try {
+        val signature = "Landroid/media/MediaCodecInfo\$CodecCapabilities"
+
+        if (Build.VERSION.SDK_INT >= 36) {
+            // LSPass will be blocked if core platform API restriction is enabled,
+            // so use HiddenApiBypass to be safe.
+            HiddenApiBypass.addHiddenApiExemptions(signature)
+        } else if (Build.VERSION.SDK_INT >= 28) {
+            val result = LSPass.addHiddenApiExemptions(signature)
+            if (!result) {
+                HiddenApiBypass.addHiddenApiExemptions(signature)
+            }
+        }
+    } catch (_: Throwable) {}
 }

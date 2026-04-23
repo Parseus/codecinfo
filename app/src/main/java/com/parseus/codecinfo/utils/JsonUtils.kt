b270fe69cf7892eb5597bf0a9ac425af05d69990
Parseus <nawrot.krzysztof@gmail.com>
2026-04-23 20:07:29 +0200

    Disable logging JSON exceptions in release builds

--- app/src/main/java/com/parseus/codecinfo/utils/JsonUtils.kt
+++ app/src/main/java/com/parseus/codecinfo/utils/JsonUtils.kt
@@ -1,7 +1,11 @@
 package com.parseus.codecinfo.utils
 
+import com.parseus.codecinfo.BuildConfig
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 
+@OptIn(ExperimentalSerializationApi::class)
 val jsonInstance = Json {
     ignoreUnknownKeys = true
+    exceptionsWithDebugInfo = BuildConfig.DEBUG
 }
